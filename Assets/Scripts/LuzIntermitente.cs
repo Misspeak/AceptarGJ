ccd360b9937265bf7dbe4a9558a0e07b1a7f8590
Afry CG <Afry CG>
2015-11-29 11:22:22 +0100

    Actualizado con todas las habitaciones

---
+++ Assets/Scripts/LuzIntermitente.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LuzIntermitente : MonoBehaviour {
+
+	public float timer;
+	private Light l;
+    public float time;
+
+	void Start () {
+		l = GetComponent<Light> ();
+	}
+
+	void FixedUpdate () {
+		timer -= Time.deltaTime * time;
+		if (timer <= 2)
+		{
+		l.enabled = !l.enabled;
+		timer = 0;
+		}
+		
+	}
+}
