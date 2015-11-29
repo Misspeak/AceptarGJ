42aad045135cb57e5f6226d3a78a86b5fe6d3bf3
creativwhisper <creativewhisperings@gmail.com>
2015-11-30 00:35:40 +0100

    Version definitiva, limpia de videos de creditos

---
+++ Assets/_Scripts/Inicio.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inicio : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey) {
+			Application.LoadLevel ("0.1 Negacion");
+		}
+	}
+}
